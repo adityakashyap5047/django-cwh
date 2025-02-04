333885f13cfa6a32d09953b29feea9bc27a8eb27
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-02-05 02:24:23 +0530

    creating the pages in django

--- first_site/first_site/views.py
+++ first_site/first_site/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello from 1st site")
+
+def about(request):
+    return HttpResponse("<h1>Hello from About</h1>")
