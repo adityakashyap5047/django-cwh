d5a02e06234d6aac1403c16089f477ede556974b
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-02-06 16:59:02 +0530

    comment-out all the code before the pipeline

--- first_site/first_site/views.py
+++ first_site/first_site/views.py
@@ -1,9 +1,11 @@
-# I have created this file  - Aditya Kumar
+# # I have created this file  - Aditya Kumar
 
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Hello from 1st site")
+# def index(request):
+#     return HttpResponse("Hello from 1st site")
 
-def about(request):
-    return HttpResponse("<h1>Hello from About</h1>")
+# def about(request):
+#     return HttpResponse("<h1>Hello from About</h1>")
+
+
