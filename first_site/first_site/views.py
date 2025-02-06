4f3cf0c10e09b322a9d6adbcc9f679664648cf47
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-02-06 20:06:32 +0530

    before starting with the templates

--- first_site/first_site/views.py
+++ first_site/first_site/views.py
@@ -8,12 +8,12 @@ from django.http import HttpResponse
 # def about(request):
 #     return HttpResponse("<h1>Hello from About</h1>")
 
+# Laying the pipeline
+# def index(request):
+#     return HttpResponse("Home")
 
-def index(request):
-    return HttpResponse("Home")
+# def  removepunc(request):
+#     return HttpResponse("remove punc <a href='/'>back</a>")
 
-def  removepunc(request):
-    return HttpResponse("remove punc <a href='/'>back</a>")
-
-def capfirst(request):
-    return HttpResponse("Capatalize first <a href='/'>back</a>")
+# def capfirst(request):
+#     return HttpResponse("Capatalize first <a href='/'>back</a>")
