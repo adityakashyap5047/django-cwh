03bbcba9d9e5d4089a06276034b7ba0abf967157
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-02-08 00:33:16 +0530

    inherit the base template

--- second_site/shop/views.py
+++ second_site/shop/views.py
@@ -7,7 +7,7 @@ def index(request):
     return render(request, 'shop/index.html')
 
 def about(request):
-    return HttpResponse("Hello from about")
+    return render(request, 'shop/about.html')
 
 def contact(request):
     return HttpResponse("Hello from contact")
