9a9d58db817739e890e4a84b7de1853e956ed2a3
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-02-08 02:32:13 +0530

    giving the data to the carousel from the database

--- second_site/shop/views.py
+++ second_site/shop/views.py
@@ -1,10 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Product
+from math import ceil
 
 # Create your views here.
 def index(request):
-    return render(request, 'shop/index.html')
+    products = Product.objects.all()
+    n = len(products)
+    nSlides = n // 4 + ceil((n/4) - (n//4))
+    params = {'product': products, 'range': range(1, nSlides), 'no_of_slides': nSlides} 
+    return render(request, 'shop/index.html', params)
 
 def about(request):
     return render(request, 'shop/about.html')
