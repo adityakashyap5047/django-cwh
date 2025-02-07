224d51cd9c06d8a32085b808de2119bae0d8177b
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-02-07 18:12:39 +0530

    laying the pipeline for shop app

--- second_site/shop/urls.py
+++ second_site/shop/urls.py
@@ -2,5 +2,11 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("", views.index, name="ShopHome")
+    path("", views.index, name="ShopHome"),
+    path("about/", views.about, name="About us"),
+    path("contact/", views.contact, name="Contact Us"),
+    path("tracker/", views.tracker, name="Tracking Status"),
+    path("search/", views.search, name="Search"),
+    path("productview/", views.prodView, name="Product View"),
+    path("checkout/", views.checkOut, name="Checkout"),
 ]
--- second_site/shop/views.py
+++ second_site/shop/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
     return render(request, 'shop/index.html')
+
+def about(request):
+    return HttpResponse("Hello from about")
+
+def contact(request):
+    return HttpResponse("Hello from contact")
+
+def tracker(request):
+    return HttpResponse("Hello from tracker")
+
+def search(request):
+    return HttpResponse("Hello from search")
+
+def prodView(request):
+    return HttpResponse("Hello from product view")
+
+def checkOut(request):
+    return HttpResponse("Hello from check out")
