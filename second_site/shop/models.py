0ebb23835c53dbf54b563d7a635052400d8602db
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-02-07 22:57:12 +0530

    modifying the database

--- second_site/shop/models.py
+++ second_site/shop/models.py
@@ -4,5 +4,12 @@ from django.db import models
 class Product(models.Model):
     product_id = models.AutoField    # models field reference
     product_name = models.CharField(max_length=50)
+    category = models.CharField(max_length=50, default="")
+    subCategory = models.CharField(max_length=50, default="")
+    price = models.IntegerField(default=0)
+    image = models.ImageField(upload_to='shop/images', default="")
     desc = models.CharField(max_length=300)
     pub_date = models.DateField()
+
+    def __str__(self):
+        return self.product_name
