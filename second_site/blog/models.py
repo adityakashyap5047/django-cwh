ebaf28bf23688282c2bdff24e3c5610904dcc8de
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-02-12 01:16:22 +0530

    making the model and register it in admin.py

--- second_site/blog/models.py
+++ second_site/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Blogpost(models.Model):
+    post_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=50)
+    head0 = models.CharField(max_length=500)
+    head1 = models.CharField(max_length=500)
+    head2 = models.CharField(max_length=500)
+    pub_date = models.DateField()
+    thumbnail = models.ImageField(upload_to='blog/images', default="")
+
+    def __str__(self):
+        return self.title
