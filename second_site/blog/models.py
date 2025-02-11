115ea375ae79513a37d8cce2aa0705ca2ab53a4c
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-02-12 01:22:40 +0530

    adding the more fields in the blog model and adding the data into database

--- second_site/blog/models.py
+++ second_site/blog/models.py
@@ -5,8 +5,11 @@ class Blogpost(models.Model):
     post_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=50)
     head0 = models.CharField(max_length=500)
+    chead0 = models.CharField(max_length=5000, default="")
     head1 = models.CharField(max_length=500)
+    chead1 = models.CharField(max_length=5000, default="")
     head2 = models.CharField(max_length=500)
+    chead2 = models.CharField(max_length=5000, default="")
     pub_date = models.DateField()
     thumbnail = models.ImageField(upload_to='blog/images', default="")
 
