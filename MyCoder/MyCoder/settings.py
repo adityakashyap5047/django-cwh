0c45f696478726f1fd15886788333b99fcb519a0
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-02-13 03:36:25 +0530

    add contact model and admin registration; enhance contact form in template

--- MyCoder/MyCoder/settings.py
+++ MyCoder/MyCoder/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'blog',
-    'home'
+    'home.apps.HomeConfig',
+    'blog.apps.BlogConfig'
 ]
 
 MIDDLEWARE = [
--- MyCoder/home/models.py
+++ MyCoder/home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    sno = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+    content = models.TextField()
+    
