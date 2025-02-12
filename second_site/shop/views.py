f0e3800098df5f2bb45d3f5f00459cbdb1a9a7c2
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-02-12 19:59:55 +0530

    add search functionality to shop with search form and view

--- second_site/shop/views.py
+++ second_site/shop/views.py
@@ -76,6 +76,25 @@ def index(request):
     
     return render(request, 'shop/index.html', params)
 
+def searchMatch(query, item):
+    return True
+
+def search(request):
+    query = request.GET.get('search')
+    allProds = []
+    catProds = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catProds}   
+    for cat in cats:
+        prods = Product.objects.filter(category=cat)
+        prod = [item for item in prods if searchMatch(query, item)]
+        n = len(prod)
+        nSlides = n // 4 + ceil((n/4) - (n//4))
+        allProds.append([prod, range(1, nSlides), nSlides])
+    
+    params = {'allProds': allProds}
+    
+    return render(request, 'shop/index.html', params)
+
 def about(request):
     return render(request, 'shop/about.html')
 
