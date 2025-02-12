f4b8f716aedae5d57cf6af89a385dfabb612c1a4
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-02-12 20:30:57 +0530

    enhance search functionality with improved query handling and dynamic product display

--- second_site/shop/views.py
+++ second_site/shop/views.py
@@ -77,7 +77,10 @@ def index(request):
     return render(request, 'shop/index.html', params)
 
 def searchMatch(query, item):
-    return True
+    if query.lower() in item.desc.lower() or query.lower() in item.product_name.lower() or query.lower() in item.category.lower():
+        return True
+    else:    
+        return False
 
 def search(request):
     query = request.GET.get('search')
@@ -89,11 +92,14 @@ def search(request):
         prod = [item for item in prods if searchMatch(query, item)]
         n = len(prod)
         nSlides = n // 4 + ceil((n/4) - (n//4))
-        allProds.append([prod, range(1, nSlides), nSlides])
+        if n != 0:
+            allProds.append([prod, range(1, nSlides), nSlides])
     
-    params = {'allProds': allProds}
+    params = {'allProds': allProds, 'msg': ""}
+    if(len(allProds) == 0 or len(query) > 4):
+        params = {'msg': "Please make sure to enter relevant search query"}
     
-    return render(request, 'shop/index.html', params)
+    return render(request, 'shop/search.html', params)
 
 def about(request):
     return render(request, 'shop/about.html')
@@ -133,9 +139,6 @@ def tracker(request):
         except Exception as e:
             return HttpResponse("{}")
     return render(request, 'shop/tracker.html')
-
-def search(request):
-    return render(request, 'shop/search.html')
 
 def prodView(request, p_id):
     # fetch the product based on the id
