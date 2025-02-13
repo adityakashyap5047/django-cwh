65a527af02f1d95ff005d7fe4c69b5ecd1a61d61
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-02-14 01:05:45 +0530

    making the comment model

--- second_site/shop/admin.py
+++ second_site/shop/admin.py
@@ -3,7 +3,8 @@ from django.contrib import admin
 # Register your models here.
 from .models import Product, Contact, Orders, OrderUpadte
 
-admin.site.register(Product)
-admin.site.register(Contact)
+# admin.site.register(Product)
+# admin.site.register(Contact)
+admin.site.register((Product, Contact)) # we can also do it like this
 admin.site.register(Orders)
 admin.site.register(OrderUpadte)
