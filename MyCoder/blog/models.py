f851f2b659e6c69caa8265f82d23ecb7cce6cb42
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-02-13 17:30:06 +0530

    adding the post table in blog app

--- MyCoder/blog/models.py
+++ MyCoder/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    sno = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=50)
+    content = models.TextField(max_length=1000)
+    author = models.CharField(max_length=30)
+    timeStamp = models.DateTimeField(blank=True)
+
+    def __str__(self):
+        return self.title + 'by' + self.author
